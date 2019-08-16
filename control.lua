e900782d552929689330e0514e983fb0d82bdc04
Oarcinae <oarcinae@gmail.com>
2019-08-16 11:30:19 -0500

    Trying to fix the weird bug with player name being a number. Changes to use player index in a LOT of places, probably introduced some new bugs. Changed gui tab to have text at the start. Change enemy bases to visibly die if being removed inside the player's spawn radius.

--- control.lua
+++ control.lua
@@ -193,6 +193,10 @@ script.on_event(defines.events.on_player_created, function(event)
     InitOarcGuiTabs(player)
 end)
 
+script.on_event(defines.events.on_player_removed, function(event)
+    OarcEnemiesPlayerRemovedEvent(event)
+end)
+
 script.on_event(defines.events.on_player_respawned, function(event)
     SeparateSpawnsPlayerRespawned(event)
 
